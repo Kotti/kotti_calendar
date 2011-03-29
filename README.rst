3af1210a7156aea88e5ec6ccb652ba1907c0a8fc
Daniel Nouri <daniel.nouri@gmail.com>
2011-03-29 15:31:35 +0200

    Initial commit

---
+++ README.rst
@@ -0,0 +1,14 @@
+==============
+kotti_calendar
+==============
+
+This is an extension to the Kotti CMS that allows you to add calendars
+with events to your Kotti site.
+
+It uses the `FullCalendar jQuery plugin`_ to display calendars.
+
+`Find out more about Kotti`_
+
+
+.. _FullCalendar jQuery plugin: http://arshaw.com/fullcalendar/
+.. _Find out more about Kotti: http://pypi.python.org/pypi/Kotti
---
+++ setup.py
@@ -0,0 +1,30 @@
+import os
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.rst')).read()
+CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
+
+setup(name='kotti_calendar',
+      version='0.1',
+      description="Add a calendar to your Kotti site",
+      long_description=README + '\n\n' + CHANGES,
+      classifiers=[
+        "Programming Language :: Python",
+        "Framework :: Pylons",
+        "Topic :: Internet :: WWW/HTTP",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Internet :: WWW/HTTP :: WSGI :: Application",
+        "License :: Repoze Public License",
+        ],
+      author='Daniel Nouri',
+      author_email='daniel.nouri@gmail.com',
+      url='http://pypi.python.org/pypi/kotti_twitter',
+      keywords='calendar fullcalendar kotti cms pylons pyramid',
+      license="BSD-derived (http://www.repoze.org/LICENSE.txt)",
+      packages=find_packages(),
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['Kotti'],
+      tests_require=['nose', 'coverage'],
+      )
