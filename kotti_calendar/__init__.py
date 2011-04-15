48f63ec3fc9b05cc449c63a9c0dade58b4eb5a07
Daniel Nouri <daniel.nouri@gmail.com>
2011-04-15 14:04:53 +0200

    This isn't the Pyramid config object, make a distinction.

--- kotti_calendar/__init__.py
+++ kotti_calendar/__init__.py
@@ -1,3 +1,3 @@
-def kotti_configure(config):
-    config['kotti.includes'] += ' kotti_calendar.views'
-    config['kotti.available_types'] += ' kotti_calendar.resources.Calendar kotti_calendar.resources.Event'
+def kotti_configure(settings):
+    settings['kotti.includes'] += ' kotti_calendar.views'
+    settings['kotti.available_types'] += ' kotti_calendar.resources.Calendar kotti_calendar.resources.Event'
