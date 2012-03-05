88995d0f7721462b38424c9812f7f1afe8ea1e8b
Daniel Nouri <daniel.nouri@gmail.com>
2012-03-05 20:56:20 +0100

    Temporarily: Include a patch for colander.

--- kotti_calendar/__init__.py
+++ kotti_calendar/__init__.py
@@ -1,3 +1,20 @@
 def kotti_configure(settings):
     settings['kotti.includes'] += ' kotti_calendar.views'
     settings['kotti.available_types'] += ' kotti_calendar.resources.Calendar kotti_calendar.resources.Event'
+
+
+def _patch_colander():
+    # https://github.com/dnouri/colander/commit/6a09583a8b9bcae29d6f51ce05434becff379134
+    from colander import null
+    from colander import DateTime
+
+    save_datetime_serialize = DateTime.serialize
+
+    def datetime_serialize(self, node, appstruct):
+        if not appstruct:
+            return null
+        return save_datetime_serialize(self, node, appstruct)
+
+    DateTime.serialize = datetime_serialize
+
+_patch_colander()
