f67dddf22e46c6f357cfcc5cdfdc4d8c66a86fee
Daniel Nouri <daniel.nouri@gmail.com>
2012-06-21 15:14:52 +0200

    Release 0.6

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ tests_require = [
     ]
 
 setup(name='kotti_calendar',
-      version='0.6dev',
+      version='0.6',
       description="Add a calendar to your Kotti site",
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
